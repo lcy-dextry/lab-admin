79cd1b0e7b83cf76408be6e8ba3d49b9d071bab2
lcy-dextry <l18711130734@163.com>
2022-12-03 17:27:24 +0800

    提交代码

--- src/App.js
+++ src/App.js
@@ -5,20 +5,13 @@ import Login from '@/pages/Login';
 import Admin from '@/pages/Admin';
 // 数据
 import { login } from '@/redux/actions';
-
 import { auth } from '@/utils/cloudBase';
 
 const App = ({ loginState, login }) => {
-  // auth
-  //   .signUpWithEmailAndPassword('172816323@qq.com', 'fluidlab123')
-  //   .then(() => {
-  //     console.log('已发送邮件验证')
-  //   });
-  // useEffect(() => {
-  //   auth.hasLoginState() ? login(true) : login(false);
-  // }, [loginState]);
-  // return <>{loginState ? <Admin /> : <Login />}</>;
-  return <Admin />
+  useEffect(() => {
+    auth.hasLoginState() ? login(true) : login(false);
+  }, [loginState]);
+  return <>{loginState ? <Admin /> : <Login />}</>;
 };
 export default connect(
   state => ({
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,8 @@ import { legacy_createStore as createStore } from 'redux';
 import allReducers from './reducers';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store =
-    process.env.NODE_ENV === 'development'
-        ? createStore(allReducers, composeWithDevTools())
-        : createStore(allReducers);
+const store = createStore(allReducers, composeWithDevTools())
+// process.env.NODE_ENV === 'development'
+//     ? createStore(allReducers, composeWithDevTools())
+//     : createStore(allReducers);
 export default store;
